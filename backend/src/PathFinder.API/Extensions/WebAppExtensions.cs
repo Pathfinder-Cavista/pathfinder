b0e23001b69bd700c4b6a1246d3292d614051ebc
Toba Ojo <tojo@axxess.com>
2025-09-16 23:26:43 +0100

    * Added global exception handler * Added hangfire dashboard configurations * Added migration runner and running

---
+++ backend/src/PathFinder.API/Extensions/WebAppExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using Microsoft.EntityFrameworkCore;
+using PathFinder.API.Filters;
+using PathFinder.Infrastructure.Persistence;
+
+namespace TalentDataTracker.API.Extensions
+{
+    public static class WebAppExtensions
+    {
+        public static WebApplication RunMigrations(this WebApplication app, bool alwayRun = false)
+        {
+            if (app.Environment.IsDevelopment() || alwayRun)
+            {
+                using var scope = app.Services.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
+            return app;
+        }
+
+        public static WebApplication UseHangfireDashboard(this WebApplication app, IConfiguration configuration)
+        {
+            app.UseHangfireDashboard("/admin/jobs", new DashboardOptions
+            {
+                Authorization = new[] { new HangfireAuthorizationFilter(configuration) },
+                DashboardTitle = "Jobs Dashboard",
+                DisplayStorageConnectionString = false,
+                DisplayNameFunc = (_, job) => job.Method.Name,
+                DarkModeEnabled = true,
+            });
+
+            return app;
+        }
+    }
+}
--- backend/src/PathFinder.API/Program.cs
+++ backend/src/PathFinder.API/Program.cs
@@ -1,13 +1,18 @@
+using PathFinder.API.Extensions;
+using TalentDataTracker.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.RegisterServices(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+app.UseGlobalExceptionHandler(logger);
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
@@ -15,7 +20,11 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseHangfireDashboard(builder.Configuration)
+    .RunMigrations();
 
 app.MapControllers();
 
