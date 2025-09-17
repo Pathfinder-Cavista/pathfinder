1fea371983af8e48815f33aa1df4f47b53068f78
Toba Ojo <tojo@axxess.com>
2025-09-17 06:18:30 +0100

    * Added sample controller * Ran automatic migration at App startup * Updated the README.md file

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# pathfinder
-Talent data tracker for efficient recruitment process and valuable insights.
+# Path Finder - Cavista Tech. Talent Data Tracker
+
+This is a lightweight application to automate the tracking of open roles, calculate the number of days they have remain open and integrate the data into dashboards for real-time insights
+
+---
+
+## Running the App
+
+From the `docker` directory (where the `docker-compose.yml` file is location), run:
+
+```bash
+docker-compose up -d db
+```
+
+Then run other services, e.g:
+
+```
+docker-compose up --build api
+```
+
+This will:
+
+- First build the Database image and ensure it is fully initialized.
+- Build the the image of the backend API
+- Start the Database and the API services defined in the `docker-compose.yml`
+
+## Accessing the Service
+
+- Once running, the API service will be available at:
+
+```
+http://localhost:5005
+```
---
+++ backend/src/PathFinder.API/Controllers/SampleController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PathFinder.API.Mappers;
+using PathFinder.API.Requests;
+using PathFinder.Application.DTOs;
+using PathFinder.Application.Exceptions;
+using PathFinder.Application.Interfaces;
+
+namespace PathFinder.API.Controllers
+{
+    [Route("api/sample")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly IServiceManager _servvice;
+
+        public SampleController(IServiceManager servvice)
+        {
+            _servvice = servvice;
+        }
+
+        /// <summary>
+        /// Creates a sample
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Sample object</returns>
+        /// <response code="200">Returns the sample object</response>
+        /// <response code="400">If the request is invalid</response>
+        /// <response code="500">If an unexpected error occurred</response>
+        [HttpPost]
+        [ProducesResponseType(typeof(SampleDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Post([FromBody] CreateSampleRequest request)
+        {
+            var command = SampleRequestMapper.ToSampleCommand(request);
+            return Ok(await _servvice.Sample.AddSample(command));
+        }
+    }
+}
