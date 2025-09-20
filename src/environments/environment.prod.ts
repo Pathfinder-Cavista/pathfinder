cb64ab1a3a89c34a62fb70e4c59d7ff2a734c44d
Chukwudi Ike-Nwako <cike-nwako@axxess.com>
2025-09-20 14:07:42 +0100

    added environment values and file replacement strategy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: "https://pathfinder-api-077t.onrender.com",
+    idPhrase: 'path-finder-id',
+    accessTokenPhrase: 'path-finder-access-token',
+    refreshTokenPhrase: 'path-finder-refresh-token',
+}
