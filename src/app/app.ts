e59e82de208a14b4259083b11f1c49bcf49498b1
Chukwudi Ike-Nwako <cike-nwako@axxess.com>
2025-09-21 00:58:41 +0100

    displaying loader when getting user details on app init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AlertComponent } from "./common/alert/alert.component";
+import { AuthService } from '../generated/services/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AlertComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit, OnDestroy {
   protected readonly title = signal('pathfinder-web');
+  protected readonly isLoading = signal(true);
+  protected readonly subscription: Subscription[] = [];
+
+  constructor(private authService: AuthService) { }
+
+  private getCurrentUser() {
+    this.authService.fetchCurrentUser().subscribe({
+      next: () => {
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.authService.logout();
+        this.isLoading.set(false);
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.forEach(s => s.unsubscribe);
+  }
 }
