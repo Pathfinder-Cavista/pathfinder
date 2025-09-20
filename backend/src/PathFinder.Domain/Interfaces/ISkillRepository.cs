8aaa5631eeebda78c26e7579ddd0634321b04f10
Toba Ojo <tojo@axxess.com>
2025-09-20 12:38:04 +0100

    Refactoring

--- backend/src/PathFinder.Domain/Interfaces/ISkillRepository.cs
+++ backend/src/PathFinder.Domain/Interfaces/ISkillRepository.cs
@@ -6,10 +6,6 @@ namespace PathFinder.Domain.Interfaces
     public interface ISkillRepository
     {
         Task AddRangeAsync(List<Skill> skills, bool save = true);
-        Task AddRangeAsync(List<TalentSkill> skills, bool save = true);
-        Task AddRangeAsync(List<JobSkill> skills, bool save = true);
         IQueryable<Skill> AsQueryable(Expression<Func<Skill, bool>> expression);
-        Task<List<JobSkill>> GetJobSkillsAsync(Expression<Func<JobSkill, bool>> expression);
-        Task<List<TalentSkill>> GetTalentSkillsAsync(Expression<Func<TalentSkill, bool>> expression);
     }
 }
--- backend/src/PathFinder.Infrastructure/Repositories/SkillRepository.cs
+++ backend/src/PathFinder.Infrastructure/Repositories/SkillRepository.cs
@@ -8,11 +8,8 @@ namespace PathFinder.Infrastructure.Repositories
 {
     public class SkillRepository : Repository<Skill>, ISkillRepository
     {
-        private readonly AppDbContext _dbContext;
-
         public SkillRepository(AppDbContext dbContext) : base(dbContext)
         {
-            _dbContext = dbContext;
         }
 
         public IQueryable<Skill> AsQueryable(Expression<Func<Skill, bool>> expression)
@@ -21,44 +18,6 @@ namespace PathFinder.Infrastructure.Repositories
         public async Task AddRangeAsync(List<Skill> skills, bool save = true)
         {
             await InsertRangeAsync(skills, save);
-        }
-
-        public async Task AddRangeAsync(List<TalentSkill> skills, bool save = true)
-        {
-            await _dbContext.AddRangeAsync(skills);
-            if (save)
-            {
-                await _dbContext.SaveChangesAsync();
-            }
-        }
-
-        public async Task AddRangeAsync(List<JobSkill> skills, bool save = true)
-        {
-            await _dbContext.AddRangeAsync(skills);
-            if (save)
-            {
-                await _dbContext.SaveChangesAsync();
-            }
-        }
-
-        public async Task<List<TalentSkill>> GetTalentSkillsAsync(Expression<Func<TalentSkill, bool>> expression)
-        {
-            var skills = await _dbContext.TalentSkills
-                .Include(ts => ts.Skill)
-                .Where(expression)
-                .ToListAsync();
-
-            return skills;
-        }
-
-        public async Task<List<JobSkill>> GetJobSkillsAsync(Expression<Func<JobSkill, bool>> expression)
-        {
-            var skills = await _dbContext.JobSkills
-                .Include(ts => ts.Skill)
-                .Where(expression)
-                .ToListAsync();
-
-            return skills;
         }
     }
 }
