3397569aa24f662bf6ba862a01ebcff79c5b2363
Toba Ojo <tojo@axxess.com>
2025-09-20 22:31:41 +0100

    Added ping endpoint

---
+++ backend/src/PathFinder.API/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PathFinder.API.Controllers
+{
+    [Route("api/ping")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
