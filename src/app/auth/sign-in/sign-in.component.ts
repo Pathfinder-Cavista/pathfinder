ab2ed776078e2370025d9775df21e71e90f6b63c
Chukwudi Ike-Nwako <cike-nwako@axxess.com>
2025-09-21 01:01:10 +0100

    utilised alerting mechanism and refacored form control name

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { AuthService } from '../../../generated/services/auth/auth.service';
 import { Router } from '@angular/router';
+import { AlertService } from '../../../generated/services/alert/alert.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -28,21 +29,23 @@ export class SignInComponent {
   };
 
   protected loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
+    emailAddress: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private alertService: AlertService) {}
 
   protected signIn() {
+    this.alertService.showInfo('Please Wait', 'Signing you in...');
     this.authService.login(this.loginForm.value).subscribe({
       next: () => {
+        this.alertService.showSuccess('Success', 'Sign in operation successful!')
         this.authService.fetchCurrentUser().subscribe({
           next: () => this.router.navigate(['/dashboard']),
-          error: () => console.warn('Failed to load user info')
+          error: (err) => this.alertService.showDanger('Error', err.error.message)
         });
       },
-      error: () => console.error('Login failed')
+      error: (err) => this.alertService.showDanger('Error', err.error.message)
     });
   }
 }
