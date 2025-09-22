19c0b2116da383ace68d440b83cbafadcc40752b
Chukwudi Ike-Nwako <cike-nwako@axxess.com>
2025-09-22 05:55:00 +0100

    fleshed out skeleton for dashbaord and layout

--- src/app/main-area/layout/layout.component.ts
+++ src/app/main-area/layout/layout.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../../generated/services/auth/auth.service';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout.component',
   imports: [
-    RouterOutlet
-  ],
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit, OnDestroy {
+  protected currentRoute: string = '';
+  protected subscription: Subscription[] = [];
 
+  constructor(protected authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.currentRoute = this.router.url;
+
+    const sub = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute = event.urlAfterRedirects;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.forEach(s => s.unsubscribe());
+  }
 }
