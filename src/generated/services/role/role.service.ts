c1daa068ac3a05b554653e175843b678a91361a1
Chukwudi Ike-Nwako <cike-nwako@axxess.com>
2025-09-24 13:20:52 +0100

    added a role service

---
+++ src/generated/services/role/role.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { ApiResponseModel, PagedResponseModel } from "../../../models/common/api-response.model";
+import { RoleDetailsModel } from "../../../models/roles/role.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  private readonly _apiBaseUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  allRoles() {
+    return this.http.get<ApiResponseModel<PagedResponseModel<RoleDetailsModel>>>(`${this._apiBaseUrl}/api/jobs`);
+  }
+}
---
+++ src/models/roles/role.model.ts
@@ -0,0 +1,13 @@
+export interface RoleDetailsModel {
+  id: string,
+  recruiterId: string,
+  title: string,
+  description: string,
+  status: string,
+  type: string,
+  level: string,
+  location: string,
+  deadLine: string,
+  dateCreated: string,
+  requiredSkills: string[]
+}
