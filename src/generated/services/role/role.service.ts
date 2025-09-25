27715c945c810c6eb23c49ddf9e699d85a2c1765
Chukwudi Ike-Nwako <cike-nwako@axxess.com>
2025-09-25 08:00:43 +0100

    added support for roles paging params in the all roles method

--- src/generated/services/role/role.service.ts
+++ src/generated/services/role/role.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { ApiResponseModel, PagedResponseModel } from "../../../models/common/api-response.model";
 import { RoleDetailsModel } from "../../../models/roles/role.model";
+import { AllRolesPagingParams } from "../../../models/common/paging-params.model";
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,18 @@ export class RoleService {
 
   constructor(private http: HttpClient) {}
 
-  allRoles() {
-    return this.http.get<ApiResponseModel<PagedResponseModel<RoleDetailsModel>>>(`${this._apiBaseUrl}/api/jobs`);
+  allRoles(pagingParams: AllRolesPagingParams) {
+    let params = new HttpParams();
+
+    if (pagingParams.sortOrder) params = params.append('sortOrder', pagingParams.sortOrder);
+    if (pagingParams.search) params = params.append('search', pagingParams.search);
+    if (pagingParams.status) params = params.append('status', pagingParams.status);
+    if (pagingParams.type) params = params.append('type', pagingParams.type);
+    if (pagingParams.level) params = params.append('level', pagingParams.level);
+    if (pagingParams.page) params = params.append('page', pagingParams.page);
+    if (pagingParams.size) params = params.append('size', pagingParams.size);
+    
+    return this.http.get<ApiResponseModel<PagedResponseModel<RoleDetailsModel>>>(`${this._apiBaseUrl}/api/jobs`, { params });
   }
 
   addRole(details: {}) {
