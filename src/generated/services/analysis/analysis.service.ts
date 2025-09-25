b3949b0e1e7566fa8362e82f92e1537941239bcb
Chukwudi Ike-Nwako <cike-nwako@axxess.com>
2025-09-25 19:07:36 +0100

    added more analysis methods

--- src/generated/services/analysis/analysis.service.ts
+++ src/generated/services/analysis/analysis.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { ApplicationsByLocationResponse, JobDistributionResponseModel } from "../../../models/analysis/analysis.model";
+import { ApplicationsByLocationResponseModel, ApplicationsByStatusResponseModel, ApplicationsPerJobResponseModel, ApplicationTrendForYearResponseModel, ApplicationTrendResponseModel, HireRateByJobtypeResponseModel, JobDistributionResponseModel, OpenRolesDurationResponseModel } from "../../../models/analysis/analysis.model";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +24,37 @@ export class AnalysisService {
 
   getApplicationsByLocation() {
     const headers = this.insightHeaders;
-    return this.http.get<ApplicationsByLocationResponse[]>(`${this._apiBaseUrl}/api/analytics/applications-by-location`, { headers });
+    return this.http.get<ApplicationsByLocationResponseModel[]>(`${this._apiBaseUrl}/api/analytics/applications-by-location`, { headers });
   }
+
+  getApplicationsPerJob() {
+    const headers = this.insightHeaders;
+    return this.http.get<ApplicationsPerJobResponseModel[]>(`${this._apiBaseUrl}/api/analytics/applications-per-job`, { headers });
+  }
+  
+  getApplicationsTrend() {
+    const headers = this.insightHeaders;
+    return this.http.get<ApplicationTrendResponseModel[]>(`${this._apiBaseUrl}/api/analytics/applications-trend`, { headers });
+  }
+  
+  getApplicationsTrendForYear(year: number) {
+    const headers = this.insightHeaders;
+    return this.http.get<ApplicationTrendForYearResponseModel[]>(`${this._apiBaseUrl}/api/analytics/applications-trend/${year}`, { headers });
+  }
+  
+  getHireRateByJobtype() {
+    const headers = this.insightHeaders;
+    return this.http.get<HireRateByJobtypeResponseModel[]>(`${this._apiBaseUrl}/api/analytics/hire-rate-by-jobtype`, { headers });
+  }
+  
+  getApplicationsByStatus() {
+    const headers = this.insightHeaders;
+    return this.http.get<ApplicationsByStatusResponseModel[]>(`${this._apiBaseUrl}/api/analytics/application-by-status`, { headers });
+  }
+  
+  getOpenRolesDuration() {
+    const headers = this.insightHeaders;
+    return this.http.get<OpenRolesDurationResponseModel[]>(`${this._apiBaseUrl}/api/analytics/open-roles-durations`, { headers });
+  }
+  
 }
--- src/models/analysis/analysis.model.ts
+++ src/models/analysis/analysis.model.ts
@@ -4,7 +4,44 @@ export interface JobDistributionResponseModel {
   count: number,
 }
 
-export interface ApplicationsByLocationResponse {
+export interface ApplicationsByLocationResponseModel {
   location: string,
   applications: number,
+}
+
+export interface ApplicationsPerJobResponseModel {
+  jobId: string,
+  jobTitle: string,
+  count: number,
+}
+
+export interface ApplicationTrendResponseModel {
+  date: string,
+  count: number,
+}
+
+export interface ApplicationTrendForYearResponseModel {
+  monthKey: number,
+  month: string,
+  applications: number,
+}
+
+export interface HireRateByJobtypeResponseModel {
+  jobType: number,
+  jobTypeText: string,
+  hires: number,
+}
+
+export interface ApplicationsByStatusResponseModel {
+  status: number,
+  statusText: string,
+  count: number,
+}
+
+export interface OpenRolesDurationResponseModel {
+  roleId: string,
+  roleTitle: string,
+  status: number,
+  statusText: string,
+  openDays: number,
 }
