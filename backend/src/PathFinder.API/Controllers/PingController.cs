fba2baff567d98372a5a6bcc267b2dacaf5d8f14
Toba Ojo <tojo@axxess.com>
2025-09-25 10:02:41 +0100

    Added permission filter to ping index and returns ok object

--- backend/src/PathFinder.API/Controllers/PingController.cs
+++ backend/src/PathFinder.API/Controllers/PingController.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PathFinder.API.Filters;
 
 namespace PathFinder.API.Controllers
 {
     [Route("api")]
     [ApiController]
+    [AnalyticsPermission]
     public class PingController : ControllerBase
     {
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok();
+            return Ok(new { status = "ok" });
         }
     }
 }
